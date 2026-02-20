9814504513f5d7f53680d4026e13b6a958ec13c4
0365jabad <0365jabad@e-itaca.es>
2026-02-20 15:44:05 +0100

    Prueba de push

--- src/Ej6/Maquinaria/Vagon.java
+++ src/Ej6/Maquinaria/Vagon.java
@@ -17,21 +17,20 @@ public class Vagon {
     private String tipo;
 
     /**
-     * Constructor, cuando se crea un vagon solo introduciré como parametro la 
-     * cargaMaxima. 
-     *  - El tipo lo desconozco, unas veces llevará una mercancía y 
-     * otras veces otra. 
-     *  - La carga Actual, nada más crearlo será 0. 
-     *  - Y el id, lo voy a calcular de manera aleatoria.
+     * Constructor, cuando se crea un vagon solo introduciré como parametro la
+     * cargaMaxima. - El tipo lo desconozco, unas veces llevará una mercancía y
+     * otras veces otra. - La carga Actual, nada más crearlo será 0. - Y el id,
+     * lo voy a calcular de manera aleatoria.
+     *
      * @param cargaMaxima
      */
     public Vagon(double cargaMaxima) {
         this.cargaMaxima = cargaMaxima;
         this.tipo = ""; //Sin mercancia
-        this.id = (int)(Math.random()*99999); //Aleatorio
+        this.id = (int) (Math.random() * 99999); //Aleatorio
         this.cargaActual = 0; //Nada más crarlo, sin carga.
     }
-    
+
     //Getters --> Todos
     public int getId() {
         return id;
@@ -51,23 +50,24 @@ public class Vagon {
 
     //Setter --> Solamente el tipo y la carga actual
     /**
-     * Este método va a pasar de setCargaActual al método cargar que pone
-     * en el enunciado. 
-     * @param cargaActual 
+     * Este método va a pasar de setCargaActual al método cargar que pone en el
+     * enunciado.
+     *
+     * @param cargaActual
      */
     public void cargar(double cargaActual) {
-        if (cargaActual < this.cargaMaxima){
+        if (cargaActual < this.cargaMaxima) {
             this.cargaActual = cargaActual;
         } else {
             System.err.println("Advertencia! Estas superando la carga máxima.");
         }
-        
+
     }
-    
+
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
-    
+
     /**
      * Método que vacia el vagón, voy a hacer uso del método cargar.
      */
@@ -75,13 +75,14 @@ public class Vagon {
         cargar(0); //Método cargar y le paso 0.
         //También podría hace: this.cargaActual = 0;
     }
-    
+
     /**
      * Método para comprobar si el vagón esta lleno.
-     * @return 
+     *
+     * @return
      */
-    public boolean estaLleno(){
-        if (this.cargaActual >= this.cargaMaxima){
+    public boolean estaLleno() {
+        if (this.cargaActual >= this.cargaMaxima) {
             System.out.println("ADVERTENCIA! El vagón esta lleno.");
             return true;
         }
@@ -92,8 +93,6 @@ public class Vagon {
     public String toString() {
         return "Vagon{" + "id=" + id + ", cargaMaxima=" + cargaMaxima + ", cargaActual=" + cargaActual + ", tipo=" + tipo + '}';
     }
-    
-    
-    
-    
+
+    //Comentario para comprobar si funciona el pull y el push
 }
